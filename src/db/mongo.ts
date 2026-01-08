e15a81809701251160d84b2591debc2b8d4b4895
SooryaCodes <sooryakriz111@gmail.com>
2026-01-09 00:01:54 +0530

    feat: setup mongodb atlas connection with types

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,11 +1,16 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, Db } from "mongodb";
 
-const uri = process.env.MONGO_URI!;
-export const client = new MongoClient(uri);
+let db: Db;
 
-export async function connectDB() {
+export async function connectDB(uri: string): Promise<Db> {
+  const client = new MongoClient(uri);
   await client.connect();
+  db = client.db("foodo_pos");
   console.log("MongoDB connected");
+  return db;
 }
 
-export const db = client.db("foodo_pos");
+export function getDB(): Db {
+  if (!db) throw new Error("DB not initialized");
+  return db;
+}
